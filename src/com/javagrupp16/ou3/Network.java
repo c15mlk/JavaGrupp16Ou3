8f1079b108de0f4fe78cfe98015967fbda41d85c
c15mlk <malu0357@student.umu.se>
2016-05-17 12:04:15 +0200

    Network Update

--- src/com/javagrupp16/ou3/Network.java
+++ src/com/javagrupp16/ou3/Network.java
@@ -2,8 +2,7 @@ package com.javagrupp16.ou3;
 
 import com.javagrupp16.ou3.entities.Node;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Marcus on 2016-05-17.
@@ -11,9 +10,38 @@ import java.util.Map;
 public class Network {
 
     private Map<Position, Node> nodes = new HashMap<Position, Node>();
-
+    private List<UUID> eventIDList = new ArrayList<UUID>();
+    private int height, width, agentProb, eventProb, numberOfTicks;
 
     public Network(int height, int width, int agentProb, int eventProb){
-        
+        this.height = height;
+        this.width = width;
+        this.agentProb = agentProb;
+        this.eventProb = eventProb;
+        for(int x = 0 ; x < width ; x++){
+            for(int y = 0 ; y < height ; y++){
+
+            }
+        }
+    }
+
+    public boolean hasNode(Position position){
+        return nodes.containsKey(position);
+    }
+
+    public Node getNode(Position position){
+        if(nodes.containsKey(position))
+            return nodes.get(position);
+        return null;
+    }
+
+    public void timeTick(){
+        for(Node n : nodes.values()){
+
+        }
+    }
+
+    public int getTime(){
+        return numberOfTicks;
     }
 }
