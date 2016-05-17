07cd081225647291d53c9cf342eb7e961c4744bf
Simon <simon.e_@hotmail.com>
2016-05-17 11:47:20 +0200

    event

--- src/com/javagrupp16/ou3/Event.java
+++ src/com/javagrupp16/ou3/Event.java
@@ -1,21 +1,27 @@
 package com.javagrupp16.ou3;
 
+import java.util.UUID;
+
 /**
  * Created by Marcus on 2016-05-17.
  **/
 public class Event{
 
-    private int id, time;
+    private int time;
+    private UUID id;
     private Position position;
 
     public Event(){
     }
-    public int getId(){
+
+    public UUID getId(){
     return id;
     }
+
     public int getTime(){
     return time;
     }
+
     public Position getPosition(){
     return position;
     }
