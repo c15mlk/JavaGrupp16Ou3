5925b8cb50d60f16e10340d4f6eb2cceabbf750b
Mirre <Mackeduki@gmail.com>
2016-05-17 10:47:15 +0200

    Test2

---
+++ src/com/javagrupp16/ou3/entities/Entity.java
@@ -0,0 +1,25 @@
+package com.javagrupp16.ou3.entities;
+
+import com.javagrupp16.ou3.Network;
+import com.javagrupp16.ou3.Position;
+
+/**
+ * Created by Marcus on 2016-05-17.
+ */
+public class Entity {
+    protected Network network;
+    private Position position;
+
+    public Entity(Network network, Position position){
+        this.network = network;
+        this.position = position;
+    }
+
+    public Position getPosition(){
+        return position;
+    }
+
+    public void setPosition(Position position){
+        this.position = position;
+    }
+}
--- src/com/javagrupp16/ou3/entities/Moveable.java
+++ src/com/javagrupp16/ou3/entities/Moveable.java
@@ -1,8 +1,27 @@
 package com.javagrupp16.ou3.entities;
 
+import com.javagrupp16.ou3.Network;
+import com.javagrupp16.ou3.Position;
+
 /**
  * Created by Marcus on 2016-05-17.
  */
-public interface Moveable {
+public abstract class Moveable extends Entity {
+
+    private int steps;
+
+    public Moveable(Network network, Position position){
+        super(network, position);
+    }
+
+    public abstract void move();
+
+    public void step(){
+        steps++;
+    }
+
+    public int getSteps(){
+        return steps;
+    }
 
 }
